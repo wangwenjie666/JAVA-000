386a172344bee1e112ee15e110c3894c589df031
wwj_g <wwj_g@126.com>
2020-12-03 00:22:24 +0800

    * TODO shardingsphere

---
+++ Week_07/homework_2.0/src/main/java/code/Application.java
@@ -0,0 +1,10 @@
+package code;
+
+/**
+ * ShardingSphere-jdbc 的 Master-Slave 功能 1）SQL 解析和事务管理，自动实现读写分离 2）解决”写完读”不一致的问题
+ *
+ * @author wangwenjie
+ * @date 2020-12-03
+ */
+public class Application {
+}
